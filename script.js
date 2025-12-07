0861a1c1bb4e83311d99b59a364b27d7154e01b2
Mohit Parmar <127684291+mrmohitparmar@users.noreply.github.com>
2025-12-07 16:38:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 function calculateArea() {
     let radius = prompt("Enter the radius of the circle:");
-    let pi = 22/7;
+    let area = Math.PI * Number(radius) * Number(radius);
 
-    let area = pi * Number(radius) * Number(radius);
-
-    // Test case ke liye exact output format
     alert(`The area of the circle with radius ${radius} is ${area.toFixed(2)}`);
 }
 
